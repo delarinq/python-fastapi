032bc2da89d3ba257106842fc9d7ad0f3a6ffdd0
Екатерина Н <deadheroes666@gmail.com>
2023-01-24 18:47:17 +0300

    updated task_1 in main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import itertools
 app = FastAPI()
 
 @app.post("/filter/case_sensitive")
-async def task_1(l: list = Body()):
+async def task_1(l: list[str] = Body()):
     for a, b in itertools.combinations(l, 2):
         if a==b:
             l = [i for i in l if a.casefold() != i.casefold()]
